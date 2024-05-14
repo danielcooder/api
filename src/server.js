8bae16ac8d4a997fc822b31916ba37a1c9588bc7
daniel <danielcooder@gmail.com>
2024-05-14 17:41:09 -0300

    perf:query params

--- src/server.js
+++ src/server.js
@@ -8,10 +8,15 @@ app.get("/message/:id/:user", (request, response) => {
     response.send(`
 
     Mensagem ID: ${id}.
-    Para o usuário: ${user}
+    Para o usuário: ${user}.
 
     `);
+});
 
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit} `);
 });
 
 const PORT = 3333;
