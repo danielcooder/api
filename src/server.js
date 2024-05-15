b97e6f40d78bec75637f0432b431dc1583fcd642
daniel <danielcooder@gmail.com>
2024-05-15 16:23:39 -0300

    perf: body params

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user, age } = request.params;
 
-    response.send(`
+app.post("/users", (request, response) => {
 
-    Mensagem ID: ${id}.
-    Para o usuário: ${user}.
-        
-    `);
-});
+    const { name, email, password } = request.body;
 
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
-
-    response.send(`Página: ${page}. Mostrar: ${limit} `);
+    response.json({ name, email, password });
 });
 
 const PORT = 3333;
