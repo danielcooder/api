1ae055176e092b5be29040c537a7021a0008fd0d
daniel <danielcooder@gmail.com>
2024-05-20 16:17:29 -0300

    perf:tratando excecoes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,17 @@
+
+const AppError = require("../utils/AppError");
+
 class UserController {
 
     create(request, response) {
 
         const { name, email, password } = request.body;
+
+        if (!name) {
+
+            throw new AppError("Nome é obrigatório!");
+
+        }
 
         response.status(201).json({ name, email, password });
 
