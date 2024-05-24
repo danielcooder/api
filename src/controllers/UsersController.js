19735438efed7cc50118ff42ed531901ec889127
daniel <danielcooder@gmail.com>
2024-05-24 16:03:07 -0300

    perf: cadastrando usuarios

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,22 +1,28 @@
 
 const AppError = require("../utils/AppError");
 
+const sqliteConnection = require("../database/sqlite/migrations/index");
 class UserController {
 
-    create(request, response) {
+    async create(request, response) {
 
         const { name, email, password } = request.body;
 
-        if (!name) {
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-            throw new AppError("Nome é obrigatório!");
+        if (checkUserExist) {
 
+            throw new AppError("Este e-mail já esta em uso.")
         }
 
-        response.status(201).json({ name, email, password });
+        await database.run
+            ("INSERT INTO users (name,email, password) VALUES (?, ?, ?)",
+                [name, email, password]
+            );
 
+        return response.status(201).json();
     }
-
 }
 
 module.exports = UserController;
